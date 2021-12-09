bec6012cd5996d92b30c7afce8026dbcdf94e1b5
salihkoc <salihkoc-e@users.noreply.github.com>
2021-12-09 17:17:46 +0300

    Add project files.

---
+++ DinamikFormElemanlari/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace DinamikFormElemanlari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(309, 105);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(531, 435);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(313, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SKOR:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(380, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(420, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "MAYIN:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(491, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(674, 66);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(94, 33);
+            this.btnUret.TabIndex = 5;
+            this.btnUret.Text = "button1";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 743);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnUret);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUret;
+    }
+}
+
---
+++ DinamikFormElemanlari/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikFormElemanlari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            int mayin1 = 0;
+            int mayin2 = 0;
+            int mayin3 = 0;
+
+            Random rnd = new Random();
+            mayin1 = rnd.Next(1, 20);
+            mayin2 = rnd.Next(21, 40);
+            mayin3 = rnd.Next(41, 50);
+
+            for (int i = 1; i <= 50; i++)
+            {
+                Button btnTemp = new Button();
+                btnTemp.Name = "btn" + i.ToString();
+                btnTemp.Size = new System.Drawing.Size(35, 35);
+                btnTemp.Text = i.ToString();
+                btnTemp.UseVisualStyleBackColor = true;
+                if (mayin1 == i || mayin2 == i || mayin3 == i)
+                {
+                    btnTemp.Tag = true;
+                }
+                else
+                {
+                    btnTemp.Tag = false;
+                }
+                btnTemp.Click += btnTemp_Click;
+                flowLayoutPanel1.Controls.Add(btnTemp);
+
+            }
+        }
+
+        private void btnTemp_Click(object sender, EventArgs e)
+        {
+
+            Button basilanButon = (Button)sender;
+            bool mayinBulundumu = (bool)basilanButon.Tag;
+
+            if (mayinBulundumu)
+            {
+                MessageBox.Show("mayını buldun iyi .. yedin");
+                basilanButon.BackColor = Color.Red;
+                int mayınInt = int.Parse(label4.Text);
+                mayınInt++;
+                label4.Text = mayınInt.ToString();
+
+            }
+            else
+            {
+                basilanButon.BackColor = Color.Green;
+                int skorInt = int.Parse(label2.Text);
+                skorInt++;
+                label2.Text = skorInt.ToString();
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
